e8552b5a6e6c7f5002463a49455cdf4f25243a2a
Anish M N <anishmn@sahaj.ai>
2024-04-20 01:50:49 +0530

    Add a funcion to return hello

--- src/main/java/com/example/jenkinsdemo/demojenkinsapi/DemoJenkinsApiApplication.java
+++ src/main/java/com/example/jenkinsdemo/demojenkinsapi/DemoJenkinsApiApplication.java
@@ -2,9 +2,15 @@ package com.example.jenkinsdemo.demojenkinsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DemoJenkinsApiApplication {
+
+	@GetMapping("/hello")
+	public static String sayHello(){
+		return "Hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJenkinsApiApplication.class, args);
