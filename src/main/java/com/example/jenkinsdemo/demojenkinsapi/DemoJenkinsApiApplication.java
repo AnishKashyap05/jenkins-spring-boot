3315824910360a16a49e89fe7d1338197715efa6
Anish M N <anishmn@sahaj.ai>
2024-04-19 19:11:50 +0530

    initial commit

---
+++ src/main/java/com/example/jenkinsdemo/demojenkinsapi/DemoJenkinsApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.jenkinsdemo.demojenkinsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsApiApplication.class, args);
+		System.out.println("Hello world ");
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsdemo/demojenkinsapi/DemoJenkinsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdemo.demojenkinsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
